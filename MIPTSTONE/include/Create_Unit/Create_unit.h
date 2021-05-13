52633266081c9248f31337df27e47ba17d541ccd
Андрей Кротов <70897625+KrotovAndrey@users.noreply.github.com>
2021-05-13 21:16:09 +0300

    Delete MIPTSTONE/include directory

--- MIPTSTONE/include/Create_Unit/Create_unit.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by krotov_ai on 21.03.2021.
-//
-
-#ifndef MIPTSTONE_CREATE_UNIT_H
-void CRunit();
-#define MIPTSTONE_CREATE_UNIT_H
-
-#endif //MIPTSTONE_CREATE_UNIT_H
--- MIPTSTONE/include/Stub/Stub.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by krotov_ai on 21.03.2021.
-//
-
-#ifndef MIPTSTONE_STUB_H
-void Stub();
-#define MIPTSTONE_STUB_H
-
-#endif //MIPTSTONE_STUB_H
